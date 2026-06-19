91e6955a0b55a0e4cd4170428f7845f3e8a8c4cc
= <=>
2026-06-19 18:41:25 +0200

    Ignore '.next', 'node_modules' and '.git' for telescope.

--- init.lua
+++ init.lua
@@ -507,11 +507,12 @@ do
     -- You can put your default mappings / updates / etc. in here
     --  All the info you're looking for is in `:help telescope.setup()`
     --
-    -- defaults = {
+    defaults = {
     --   mappings = {
     --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
     --   },
-    -- },
+      file_ignore_patterns = { ".next", "node_modules", ".git" }
+    },
     -- pickers = {}
     extensions = {
       ['ui-select'] = { require('telescope.themes').get_dropdown() },
